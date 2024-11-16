a471c01f0f457bfc98c9d29f93553f70a65f09af
Bruce Eckel <mindviewinc@gmail.com>
2024-11-15 18:35:06 -0700

    Tuned colors & appearance

--- src/pybooktools/reminder.py
+++ src/pybooktools/reminder.py
@@ -1,21 +1,23 @@
 from rich.console import Console
+from rich.panel import Panel
 
 console = Console()
 
 
 def main() -> None:
-    console.print("[green1]Available commands:[/green1]")
-    console.print(
-        "  [deep_sky_blue1]slug[/deep_sky_blue1]   [yellow]Add/update sluglines in Python files[/yellow]"
+    content = (
+        "[deep_sky_blue1]slug[/deep_sky_blue1]    [yellow]Add/update sluglines in Python files[/yellow]\n"
+        "[deep_sky_blue1]upcon[/deep_sky_blue1]   [yellow]Update[/yellow] [green]'console =='[/green] [yellow]statements in Python files[/yellow]\n"
+        "[deep_sky_blue1]uplist[/deep_sky_blue1]  [yellow]Update code listings in Markdown files[/yellow]\n"
+        "[deep_sky_blue1]chapz[/deep_sky_blue1]   [yellow]Renumber Markdown chapters & align chapter names[/yellow]"
     )
     console.print(
-        "  [deep_sky_blue1]upcon[/deep_sky_blue1]  [yellow]Update[/yellow] 'console ==' [yellow]statements in Python files[/yellow]"
-    )
-    console.print(
-        "  [deep_sky_blue1]uplist[/deep_sky_blue1] [yellow]Update code listings in Markdown files[/yellow]"
-    )
-    console.print(
-        "  [deep_sky_blue1]chapz[/deep_sky_blue1]  [yellow]Renumber Markdown chapters & align chapter names[/yellow]"
+        Panel(
+            content,
+            border_style="green1",
+            title="Commands",
+            title_align="left",
+        )
     )
 
 
