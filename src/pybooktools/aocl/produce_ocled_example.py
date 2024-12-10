2f1c16f3d16712dacee2287b382c3319f55bf19e
Bruce Eckel <mindviewinc@gmail.com>
2024-12-10 14:15:36 -0700

    tweaks

--- src/pybooktools/aocl/produce_ocled_example.py
+++ src/pybooktools/aocl/produce_ocled_example.py
@@ -10,10 +10,8 @@ def main() -> None:
     parser = ArgumentParser(description="Add OCLs to a Python example")
     parser.add_argument("pyfile", type=str, help="The Python example file")
     args = parser.parse_args()
-
     example_path = Path(args.pyfile).resolve()
-
-    check_dir = cleaned_dir(example_path, ".check")
+    check_dir = cleaned_dir(example_path, ".check_")
 
     def write_with_ext(python_script: str, ext: str) -> Path:
         outpath = check_dir / f"{example_path.stem}_{ext}.py"
@@ -24,6 +22,7 @@ def main() -> None:
     with_ocls = add_ocl(original_source)
     write_with_ext(with_ocls, "ocls")
     embedded_ocl_results = embed_ocl_results(with_ocls)
+    outfile = check_dir / f"{example_path.stem}_ocled.py"
 
     ocl_generator = f'''
 from pybooktools.aocl import ocl_format
@@ -31,13 +30,14 @@ from pathlib import Path
 
 {with_ocls}
 
-outfile = Path("{check_dir}") / "{example_path.stem}_ocled.py"
+outfile = Path(r"{outfile}")
 outfile.write_text(f"""\
 {embedded_ocl_results}\
 """, encoding="utf-8")
     '''
 
     run_script(write_with_ext(ocl_generator, "generator"))
+    print(outfile.read_text(encoding="utf-8"))
 
 
 if __name__ == "__main__":
