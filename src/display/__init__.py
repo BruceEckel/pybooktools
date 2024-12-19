da60185f52097e472b3c664fdbc4a36bff58b610
Bruce Eckel <mindviewinc@gmail.com>
2024-12-19 13:14:08 -0700

    Create __init__.py

---
+++ src/display/__init__.py
@@ -0,0 +1,8 @@
+"""
+Standardized nice display functions.
+Works particularly well with classes that define a __rich__ method.
+https://rich.readthedocs.io
+Use:
+- Panel.fit
+- Possibly tables: https://rich.readthedocs.io/en/stable/tables.html
+"""
