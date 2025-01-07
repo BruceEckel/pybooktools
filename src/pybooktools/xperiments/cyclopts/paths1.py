5e1dd43b2d93fbcec29c0d7d0d3e2cbfa5d825e2
Bruce Eckel <mindviewinc@gmail.com>
2025-01-06 22:35:46 -0700

    Update paths1.py

--- src/pybooktools/xperiments/cyclopts/paths1.py
+++ src/pybooktools/xperiments/cyclopts/paths1.py
@@ -1,5 +1,6 @@
 # paths.py
 from itertools import product
+from pathlib import Path
 
 from cyclopts import App
 from cyclopts.types import File, ExistingFile, ResolvedFile, ResolvedExistingFile
@@ -7,35 +8,46 @@ from rich.console import Console
 from rich.panel import Panel
 
 console = Console()
+
 app = App(
     version_flags=[],
     console=console,
     help_flags="-h",
 )
 
+display = True
+
+
+def result(files: list[Path]):
+    global display
+    r = f"result: {files}"
+    if display:
+        print(f"result: {files}")
+    return str(files)
+
 
 @app.command
 def f1(files: list[File]):
-    print(f"f1: {files}")
-    return f"f1: {files}"
+    """cyclopts.types.File"""
+    return result(files)
 
 
 @app.command
 def f2(files: list[ExistingFile]):
-    print(f"f2: {files}")
-    return f"f2: {files}"
+    """cyclopts.types.ExistingFile"""
+    return result(files)
 
 
 @app.command
 def f3(files: list[ResolvedFile]):
-    print(f"f3: {files}")
-    return f"f3: {files}"
+    """cyclopts.types.ResolvedFile"""
+    return result(files)
 
 
 @app.command
-def f4(files: ResolvedExistingFile):
-    print(f"f4: {files}")
-    return f"f4: {files}"
+def f4(files: list[ResolvedExistingFile]):
+    """cyclopts.types.ResolvedExistingFile"""
+    return result(files)
 
 
 @app.command(name="-x")
@@ -53,9 +65,17 @@ def examples():
             ["paths.py", "paths.py", "nonexistent3.py", "nonexistent4.py"],
         ]
     )
-
+    global display
+    display = False
     for cmdlist in [[command] + args for command, args in all_combinations]:
-        console.print(Panel(app(cmdlist), title=str(cmdlist), style="bold"))
+        console.print(
+            Panel(
+                f"[dark_goldenrod]{app(cmdlist)}",
+                title=f"[sea_green1]{str(cmdlist)}",
+                style="blue",
+                title_align="left"
+            )
+        )
 
 
 if __name__ == "__main__":
